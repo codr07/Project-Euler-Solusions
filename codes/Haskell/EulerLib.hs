6e52e9a1791a39d4fe03fc6ae7f1dda162bffaad
Sankha Saha (CODR) <sankhasaha420@gmail.com>
2025-12-06 01:32:40 +0530

    </>

--- codes/Haskell/EulerLib.hs
+++ codes/Haskell/EulerLib.hs
@@ -7,8 +7,8 @@
  -}
 
 module EulerLib
-	(boolToInt, count, factorial, binomial, digitSum, sqrt, isPrime)
-	where
+        (boolToInt, count, factorial, binomial, digitSum, sqrt, isPrime)
+        where
 
 import Prelude hiding (sqrt)
 import Data.Bits
@@ -35,18 +35,18 @@ binomial n k = div (product [(toInteger (n - k + 1)) .. (toInteger n)]) (factori
 
 digitSum :: Integral a => a -> a
 digitSum 0 = 0
-digitSum n = (mod n 10) + (digitSum (div n 10))
+digitSum n = mod n 10 + digitSum (div n 10)
 
 
 -- sqrt n = floor(sqrt(n)).
 -- Implemented entirely in integer arithmetic; guaranteed no rounding error.
 sqrt :: (Integral a, Data.Bits.Bits a) => a -> a
 sqrt n = sqrtAlpha 1 where
-	sqrtAlpha i
-		| i * i > n = sqrtBeta (Data.Bits.shiftR i 1) 0
-		| otherwise = sqrtAlpha (Data.Bits.shiftL i 1)
-	sqrtBeta 0 acc = acc
-	sqrtBeta i acc = sqrtBeta (div i 2) (if (i + acc)^2 <= n then i + acc else acc)
+        sqrtAlpha i
+                | i * i > n = sqrtBeta (Data.Bits.shiftR i 1) 0
+                | otherwise = sqrtAlpha (Data.Bits.shiftL i 1)
+        sqrtBeta 0 acc = acc
+        sqrtBeta i acc = sqrtBeta (div i 2) (if (i + acc)^2 <= n then i + acc else acc)
 
 
 isPrime :: (Integral a, Data.Bits.Bits a) => a -> Bool
